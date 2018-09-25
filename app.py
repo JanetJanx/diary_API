d670d6ef3f1fe7ac35413f7ca9475f3c14ca75ed
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-25 12:38:19 +0300

    post method using oop implementation

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request, make_response
+from flask_restful import Resource, Api
+
 from datetime import datetime
 
 app = Flask(__name__)
+api = Api(app)
 
 entries = [
     {
@@ -22,44 +25,46 @@ entries = [
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
-@app.route('/api/v1/entries', methods=['GET'])
-def get_all_entries():
-    return make_response(jsonify(
-        {'entries':entries},
-        {"message": "Entries successfully fetched"}), 201)
-
-@app.route('/api/v1/entries/<int:entryid>', methods=['GET'])
-def get_specific_entry(entryid):
-    entry = [eid for eid in entries if eid['entryId'] == entryid]
-    return make_response(jsonify(
-        {'entry': entry[0]},
-        {"message": "Entry successfully fetched"}), 200)
-
-@app.route('/api/v1/entries', methods=['POST'])
-def add_entry():
-    try:
-        entrydata = request.get_json()
-        title = entrydata.get('title')
-        content = entrydata.get('content')
-
-        entrycontent = {
-            "entryId":entries[-1]['entryId'] + 1,
-            "title": title,
-            "content":content,
-            "time":get_timestamp()
-            }
-
-        entries.append(entrycontent)
-
+class EntryView(Resource):
+    def get(self):
         return make_response(jsonify(
-            {'entrycontent': entrycontent},
-            {'message': "Entry successfully added"}), 200)
-    
+            {'entries':entries},
+            {"message": "Entries successfully fetched"}), 201)
 
-    except (ValueError, KeyError, TypeError):
+    def post(self):
+        try:
+            entrydata = request.get_json()
+            title = entrydata.get('title')
+            content = entrydata.get('content')
+
+            entrycontent = {
+                "entryId":entries[-1]['entryId'] + 1,
+                "title": title,
+                "content":content,
+                "time":get_timestamp()
+                }
+
+            entries.append(entrycontent)
+
+            return make_response(jsonify(
+                {'entrycontent': entrycontent},
+                {'message': "Entry successfully added"}), 200)
+
+        except (ValueError, KeyError, TypeError):
+            return make_response(jsonify(
+                {'message': "JSON Format Error"}), 400)
+
+
+class SpecificEntry(Resource):
+    def get(self, entryid):
+        entry = [eid for eid in entries if eid['entryId'] == entryid]
         return make_response(jsonify(
-            {'message': "JSON Format Error"}), 400)
+            {'entry': entry[0]},
+            {"message": "Entry successfully fetched"}), 200)
 
+
+api.add_resource(EntryView, '/api/v1/entries', methods=['GET', 'POST'])
+api.add_resource(SpecificEntry, '/api/v1/entries/<int:entryid>', methods=['GET'])
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5004)
