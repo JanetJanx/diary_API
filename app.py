aa7d2063f1e5917b7a9e8fd73fffd6db9ecd605e
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-25 20:37:00 +0300

    use class instead of list only

--- app.py
+++ app.py
@@ -4,27 +4,42 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-entries = [
-    {
-        'entryId':1,
-        'title':'dictionary function',
-        'content':'using comprehenion',
-        'time':'2018-09-14 11:31:09.318212'
-        
-    },
-    {
-        'entryId':2,
-        'title':'bankaccount class',
-        'content':'class implementation corresponding to the provided tests',
-        'time':'2018-09-21 10:1:09.318212'
-    }
-        ]
+class CounterfeitEntryError(Exception):
+    pass
 
+"""class to define the entry model"""
+class Entry:
+    entryId = 0
+
+    def __init__(self, title, content, time):
+        self.entryId = None
+        self.title = title
+        self.content = content
+        self.time = time
+
+    def add(self):
+        entries_present = EntryView.entries
+
+        for entry in entries_present:
+            if entry['title'] == self.title:
+                raise CounterfeitEntryError('Entry' + {self.title} + 'already exists')
+
+        Entry.entryId + 1
+        self.entryId = Entry.entryId
+
+        entry_data = {
+            'entryId': self.entryId,
+            'title': self.title,
+            'content': self.content,
+            'time': self.time
+        }
+        entries_present.append(entry_data)
 
 class EntryView(Resource):
+    entries = []
     def get(self):
         return make_response(jsonify(
-            {'entries':entries},
+            {'entries':EntryView.entries},
             {"message": "Entries successfully fetched"}), 201)
 
 api.add_resource(EntryView, '/api/v1/entries', methods=['GET'])
