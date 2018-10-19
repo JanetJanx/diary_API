55e8d8a03875eb086ff693d67372a088aa561a48
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-10-19 16:48:17 +0300

    modify post, put and delete methods

--- app/entryapp.py
+++ app/entryapp.py
@@ -69,42 +69,41 @@ class ViewSpecificEntry(Resource):
     @classmethod
     def get(self, entryid):
         entries = GetAllEntries.entries
-        entry = [eid for eid in entries if eid['entryId'] == entryid]
-        return make_response(jsonify(
-            {'entry': entry[0]},
-            {"message": "Entry successfully fetched"}), 200)
+        for eid in entries:
+            if eid['entryId'] == entryid:
+                return make_response(jsonify(
+                    {'entry': eid},
+                    {"message": "Entry successfully fetched"}), 200)
 
 class DeleteSpecificEntry(Resource):
     """delete a specify entry"""
     @classmethod
     def delete(self, entryid):
-        entry = [eid for eid in GetAllEntries.entries if eid['entryId'] == entryid]
-        GetAllEntries.entries.remove(entry[0])
+        for eid in GetAllEntries.entries:
+            if eid['entryId'] == entryid:
+                GetAllEntries.entries.remove(eid)
         return make_response(jsonify(
-            {'entry': entry[0]},
+            {'entry': eid},
             {"message": "Entry successfully removed"}), 200)
 
 class ModifySpecificEntry(Resource):
     """modify a specific entry"""
     @classmethod
     def put(self, entryid):
-        entry = [entry for entry in GetAllEntries.entries if entry['entryId'] == entryid]
-        try:
-            entrydata = request.get_json()
-            title = entrydata.get('title')
-            content = entrydata.get('content')
+        #entry = [entry for entry in GetAllEntries.entries if entry['entryId'] == entryid]
+        for entry in GetAllEntries.entries:
+            if entry['entryId'] == entryid:
+                entrydata = request.get_json()
+                title = entrydata.get('title')
+                content = entrydata.get('content')
 
-            entry[0]['title'] = title
-            entry[0]['content'] = content
-            entry[0]['time'] = get_timestamp()
+                entry['title'] = title
+                entry['content'] = content
+                entry['time'] = get_timestamp()
 
-            return make_response(jsonify(
-                {'entry':entry[0]},
-                {'message': "Entry successfully updated"}), 202)
-
-        except (ValueError, KeyError, TypeError):
-            return make_response(jsonify(
-{'message': "JSON Format Error"}), 401)
+                return make_response(jsonify(
+                    {'entry':entry[0]},
+                    {'message': "Entry successfully updated"}), 200)
 
 
 api.add_resource(GetAllEntries, '/api/v1/entries', methods=['GET'])
